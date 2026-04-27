5ba02f58e6496fff5a1fd0668535d1d6a278c30e
Ale <aruizbermudez05@gamil.com>
2026-04-27 19:48:08 +0200

    added

---
+++ BDD/bdd.sql
@@ -0,0 +1,35 @@
+--Base de Datos project-Manager
+CREATE DATABASE proyecto-PM;
+USE proyecto-PM;
+
+--Tablas
+
+--Project
+CREATE TABLE Project(
+    id_Pro INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    descr VARCHAR(100) NOT NULL,
+    url VARCHAR(100) NOT NULL,
+);
+
+--User
+CREATE TABLE User(
+    id_U INT AUTO_INCREMENT PRIMARY KEY,
+    name_tag VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    mail VARCHAR(100) NOT NULL,
+    pass VARCHAR(50) NOT NULL,
+    UNIQUE (name_tag, mail)
+);
+
+--Permission
+CREATE TABLE Permission(
+    id_Per INT AUTO_INCREMENT PRIMARY KEY,
+    fk_Pro INT,
+    fk_U INT,
+    per INT NOT NULL, -- Los permisos son un codigo numerico 
+    CONSTRAINT fk_Pro FOREIGN KEY (fk_Pro)
+        REFERENCES Project(id_Pro),
+    CONSTRAINT fk_U FOREIGN KEY (fk_U)
+        REFERENCES User(id_U)
+);
